2389528ab5ff97ffc311d97ed157e29ca58a9054
Tommi Hämäläinen <tommihmli@gmail.com>
2025-10-02 09:18:41 +0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hei olen Tulostin-ohjelma");
+        System.out.println("Ohjelman tekijä: Tommi");
+        int luku1 = 5;
+        int luku2 = 2;
+        System.out.println("Luku1-muuttujan arvo on " + luku1);
+        System.out.println("Luku2-muuttujan arvo on " + luku2);
+        int tulo = luku1*luku2;
+        System.out.println(luku1 + "*" + luku2 + "=" + tulo);
+        
+
+    }
+}
